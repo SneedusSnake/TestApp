ac21764bee5fc59a9c813c00d3637b3e73582cce
Anton <antong@tobonet.com>
2024-03-28 17:45:01 +0400

    add migrations, models and resources for clients, sales and countries

---
+++ database/migrations/2024_03_28_102118_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->unsignedInteger('country_id');
+            $table->string('email')->unique();
+            $table->string('website')->unique();
+            $table->timestamps();
+
+            $table->foreign('country_id')->references('id')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
