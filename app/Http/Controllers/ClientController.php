7493613da0800b179439d0b4aa13feae6166e952
Anton <antong@tobonet.com>
2024-03-30 15:55:20 +0400

    move validation to Request

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Rules\Domain;
-use App\Util\DomainValidator;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Http\Requests\CreateClientRequest;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Models\Client;
 use App\Http\Resources\ClientResource;
@@ -14,13 +11,6 @@ use Illuminate\Validation\ValidationException;
 
 class ClientController extends Controller
 {
-    use ValidatesRequests;
-
-    public function __construct(private DomainValidator $domainValidator)
-    {
-
-    }
-
     public function list(): JsonResponse
     {
         return response()->json(
@@ -31,15 +21,8 @@ class ClientController extends Controller
     /**
      * @throws ValidationException
      */
-    public function create(Request $request): JsonResponse
+    public function create(CreateClientRequest $request): JsonResponse
     {
-        $this->validate($request, [
-            'first_name' => 'required',
-            'last_name'  => 'required',
-            'email'      => 'required|unique:clients',
-            'website'    => ['required', 'unique:clients', 'url', new Domain($this->domainValidator)],
-            'country_id' => 'required|exists:countries,id',
-        ]);
         $client = new Client();
         $client->setRawAttributes(
             $request->all(['first_name', 'last_name', 'email', 'website', 'country_id'])
---
+++ app/Http/Requests/CreateClientRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\Domain;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateClientRequest extends FormRequest
+{
+    public function __construct(private Domain $domainRule)
+    {
+        parent::__construct();
+    }
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required',
+            'last_name'  => 'required',
+            'email'      => 'required|unique:clients',
+            'website'    => ['required', 'unique:clients', 'url', $this->domainRule],
+            'country_id' => 'required|exists:countries,id',
+        ];
+    }
+}
