186c8388592c9513140bdd23d0cb63bc152b4ec8
Anton <antong@tobonet.com>
2024-03-31 15:27:00 +0400

    refactor DomainsDBValidatorTest

--- tests/Unit/DomainsDBValidatorTest.php
+++ tests/Unit/DomainsDBValidatorTest.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 
@@ -14,6 +15,7 @@ class DomainsDBValidatorTest extends TestCase
 {
     private Client $client;
     private MockHandler $mockHandler;
+    private DomainsDBValidator $validator;
     private array $requests = [];
 
     protected function setUp(): void
@@ -25,63 +27,40 @@ class DomainsDBValidatorTest extends TestCase
             'handler' => $handlerStack,
             'base_uri' => 'www.testing.com/',
         ]);
+        $this->validator = new DomainsDBValidator($this->client);
     }
     public function test_returns_false_if_response_is_empty(): void
     {
-        $domainNotFoundResponse = new Response(
-            200,
-            ['Content-Type' => 'application/json'],
-            file_get_contents(__DIR__ . '/../Resources/Responses/domainsdb/domain_not_found.json')
-        );
-        $this->mockHandler->append($domainNotFoundResponse);
-        $validator = new DomainsDBValidator($this->client);
+        $this->mockDomainNotFoundResponse();
 
-        $this->assertFalse($validator->validate('example.org'));
+        $this->assertFalse($this->validator->validate('example.org'));
     }
 
     public function test_makes_http_request_to_search_domains(): void
     {
-        $domainNotFoundResponse = new Response(
-            200,
-            ['Content-Type' => 'application/json'],
-            file_get_contents(__DIR__ . '/../Resources/Responses/domainsdb/domain_not_found.json')
-        );
-        $this->mockHandler->append($domainNotFoundResponse);
-        $validator = new DomainsDBValidator($this->client);
+        $this->mockDomainNotFoundResponse();
 
-        $validator->validate('http://example.org');
+        $this->validator->validate('http://example.org');
 
         $this->assertNotEmpty($this->requests);
         $this->assertEquals(
             'www.testing.com/v1/domains/search?domain=example.org',
-            $this->requests[0]['request']->getUri()->__toString()
+            $this->getRequest()->getUri()->__toString()
         );
     }
 
     public function test_returns_true_if_domain_is_found(): void
     {
-        $domainsResponse = new Response(
-            200,
-            ['Content-Type' => 'application/json'],
-            file_get_contents(__DIR__ . '/../Resources/Responses/domainsdb/domains_search.json')
-        );
-        $this->mockHandler->append($domainsResponse);
-        $validator = new DomainsDBValidator($this->client);
+        $this->mockDomainListResponse();
 
-        $this->assertTrue($validator->validate('http://wikipedia-russia.ru/articles'));
+        $this->assertTrue($this->validator->validate('http://wikipedia-russia.ru/articles'));
     }
 
     public function test_returns_false_if_domain_is_not_found(): void
     {
-        $domainsResponse = new Response(
-            200,
-            ['Content-Type' => 'application/json'],
-            file_get_contents(__DIR__ . '/../Resources/Responses/domainsdb/domains_search.json')
-        );
-        $this->mockHandler->append($domainsResponse);
-        $validator = new DomainsDBValidator($this->client);
+        $this->mockDomainListResponse();
 
-        $this->assertFalse($validator->validate('not-wikipedia-russia.ru'));
+        $this->assertFalse($this->validator->validate('not-wikipedia-russia.ru'));
     }
 
     public function urlsToParse(): array
@@ -103,30 +82,60 @@ class DomainsDBValidatorTest extends TestCase
      */
     public function test_parses_correct_domain_from_url(string $url): void
     {
-        $domainNotFoundResponse = new Response(
-            200,
-            ['Content-Type' => 'application/json'],
-            file_get_contents(__DIR__ . '/../Resources/Responses/domainsdb/domain_not_found.json')
-        );
-        $this->mockHandler->append($domainNotFoundResponse);
-        $validator = new DomainsDBValidator($this->client);
+        $this->mockDomainNotFoundResponse();
 
-        $validator->validate($url);
+        $this->validator->validate($url);
 
         $this->assertNotEmpty($this->requests);
         $this->assertEquals(
             'www.testing.com/v1/domains/search?domain=example.org',
-            $this->requests[0]['request']->getUri()->__toString()
+            $this->getRequest()->getUri()->__toString()
         );
     }
 
     public function test_throws_exception_on_service_error(): void
     {
-        $this->mockHandler->append(new Response(403));
-        $validator = new DomainsDBValidator($this->client);
+        $this->mockErrorResponse(403);
 
         $this->expectException(\Throwable::class);
 
-        $validator->validate('test');
+        $this->validator->validate('test');
+    }
+
+    private function mockDomainNotFoundResponse(): void
+    {
+        $response = new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $this->loadMockResponseContent('domain_not_found.json')
+        );
+        $this->mockHandler->append($response);
+    }
+
+    private function mockDomainListResponse(): void
+    {
+        $response = new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $this->loadMockResponseContent('domains_search.json')
+        );
+        $this->mockHandler->append($response);
+    }
+
+    private function mockErrorResponse(int $statusCode = 500): void
+    {
+        $this->mockHandler->append(new Response($statusCode));
+    }
+
+    private function loadMockResponseContent(string $fileName): string
+    {
+        return file_get_contents(
+            __DIR__ . '/../Resources/Responses/domainsdb/' . $fileName
+        );
+    }
+
+    private function getRequest(): Request
+    {
+        return $this->requests[0]['request'];
     }
 }
